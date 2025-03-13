1a98925c1676964d1a88bede1e9a6e3017d364c4
iamraguls <raguls4477@gmail.com>
2025-03-13 22:34:41 +0530

    init

---
+++ src/test/java/com/project/PatientManagementSystem/PatientManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.PatientManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
